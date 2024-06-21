599472116cd513a99e3beea75383c0eb8f129ae2
Ritista Shrestha <ritista99@gmail.com>
2024-06-21 13:17:06 +0545

    first commit

---
+++ header.jsx
@@ -0,0 +1,14 @@
+function Header(){
+    let a={
+        backgroundColor:"blue",
+        color: "wheat",
+        fontSize:"20px"
+    }
+    return(
+    <>
+    <h1 style={{color: "red"}}>This is Header</h1>
+    <p style={a}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui accusamus, vero odit dolores animi at quod porro omnis perspiciatis, quaerat minus mollitia expedita accusantium odio, dolor corrupti sed ad hic.</p>
+    </>
+    )
+}
+export default Header;
